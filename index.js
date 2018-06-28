f1e851693b96bd0034d8a68b8499e450833f4b28
akimess <akimess@gmail.com>
2018-06-28 13:49:08 +0300

    Added Web Playback SDK functionality

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/login', function(req, res) {
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: 'streaming user-read-birthdate user-read-email user-read-private',
       redirect_uri
     }));
 });
--- src/components/Info.vue
+++ src/components/Info.vue
@@ -6,33 +6,72 @@
             <p>Name: {{user_info.display_name}}</p>
             <p>Email: {{user_info.email}}</p>
         </div>
+
+        <div v-if="track_info !== null">
+          <h3>You're currently listening to:</h3>
+          <img :src="track_info.album.images[0].url" />
+          <p>{{track_info.name}} by {{track_info.artists[0].name}}</p>
+        </div>
     </div>
 </template>
 
 <script>
+window.onSpotifyWebPlaybackSDKReady = () => {};
+
 export default {
   name: "info",
   data: () => ({
     user_info: null,
-    access_token: ""
+    access_token: "",
+    track_info: null
   }),
 
-  created() {
+  async created() {
     const urlString = window.location.href;
     const url = new URL(urlString);
     this.access_token = url.searchParams.get("access_token");
 
     if (this.access_token) {
-      fetch("https://api.spotify.com/v1/me", {
+      this.user_info = await fetchAsync("https://api.spotify.com/v1/me", {
         headers: {
           Authorization: "Bearer " + this.access_token
         }
-      })
-        .then(response => response.json())
-        .then(response => {
-          this.user_info = response;
-        });
+      });
+
+      const { Player } = await waitForSpotifyWebPlaybackSDKToLoad();
+      const sdk = new Player({
+        name: "Web Playback SDK",
+        volume: 1.0,
+        getOAuthToken: callback => {
+          callback(this.access_token);
+        }
+      });
+
+      sdk.on("player_state_changed", state => {
+        // Update UI with playback state changes
+        //console.log("STATE CHANGED", state);
+        this.track_info = state.track_window.current_track;
+      });
+
+      sdk.connect();
     }
   }
 };
+
+async function fetchAsync(url, options) {
+  const response = await fetch(url, options);
+  return await response.json();
+}
+
+async function waitForSpotifyWebPlaybackSDKToLoad() {
+  return new Promise(resolve => {
+    if (window.Spotify) {
+      resolve(window.Spotify);
+    } else {
+      window.onSpotifyWebPlaybackSDKReady = () => {
+        resolve(window.Spotify);
+      };
+    }
+  });
+}
 </script>
